d03bd6c94d193c3ee48b4ea859f580a09bcae41c
Alan Verissimo <alan_verissimo@hotmail.com>
2022-11-03 14:48:15 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+
+function getApiAluno() {
+    fetch('https://jsonplaceholder.typicode.com/posts/1')
+    .then((response) => response.json())
+    .then((json) => console.log(json));
+
+}
+
+function postApiAluno() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+}
+
+    getApiAluno();
+ 
