8aa49338a8ab9d99301f3ef4c6a4d92b95e2149c
Alan Verissimo <alan_verissimo@hotmail.com>
2022-11-09 12:12:51 -0300

    commit

--- script.js
+++ script.js
@@ -1,17 +1,35 @@
+const referencia = document.getElementById("test");
+const verAluno = document.getElementById("aluno");
 
-function getApiAluno() {
-    fetch('https://jsonplaceholder.typicode.com/posts/1')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+const geral = (numerototal = 5) => {
+    fetch('https://jsonplaceholder.typicode.com/todos')
+        .then((response) => response.json())
+        .then((Json) => {
+            for (let x of Json) {
+                const arr = document.createElement("p");
+                arr.innerText = x.userId;
+                referencia.append(arr);
 
-}
+            }
+        })
 
-function postApiAluno() {
-    fetch('https://jsonplaceholder.typicode.com/posts')
+
+};
+verAluno.addEventListener('click', () => {
+    geral(5);
+
+
+})
+
+const verAluno1 = document.getElementById("aluno")
+
+fetch('https://jsonplaceholder.typicode.com/posts')
   .then((response) => response.json())
   .then((json) => console.log(json));
-  
-}
 
-    getApiAluno();
- 
+  const users = document.createElement("p1")
+
+
+
+
+  
