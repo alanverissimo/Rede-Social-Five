ebd537f677969f4af0e4403b4fdb6853647fc593
Alan Verissimo <alan_verissimo@hotmail.com>
2022-11-22 12:23:30 -0300

    final commit

--- script.js
+++ script.js
@@ -1,17 +1,60 @@
-const referencia = document.getElementById("test");
-const verAluno = document.getElementById("aluno");
+const referencia = document.getElementById("test"); //pega o elemento a partir do seu id
+const verAluno = document.getElementById("aluno"); //pega o elemento a partir do seu id
+const referencia2 = document.getElementById("postusers"); //pega o elemento a partir do seu id
 
-const geral = (numerototal = 5) => {
-    fetch('https://jsonplaceholder.typicode.com/todos')
-        .then((response) => response.json())
+
+const geral = (numerototal = 5) => { //constante ''geral''
+    fetch('https://jsonplaceholder.typicode.com/todos') //busca do endereço
+        .then((response) => response.json()) //espera resposta do json
         .then((Json) => {
             for (let x of Json) {
-                const arr = document.createElement("p");
-                arr.innerText = x.userId;
-                referencia.append(arr);
+                const fila = document.createElement("p"); //variavel arr referenciada ao elemento client
+                fila.innerText = x.userId; // está vinculando o x.userID dentro do texto da variavel 'fila'
+                fila.classList.add("client"); // adicionando classename no array que é um "client"
+                referencia.append(fila); //                 
+
+
+
+
+                fila.addEventListener('click', () => {
+                    fetch(`https://jsonplaceholder.typicode.com/posts/${x.userId}`) // busca do endereço post dos usuarios
+                        .then((response) => response.json())
+                        .then((json) => console.log(json));
+                    for (let x of Json) {
+                        const viewUsers = document.createElement("p");
+                        viewUsers.innerText = x.userId;
+                        viewUsers.classList.add("client")
+                        referencia.append(viewUsers);
+                    }
+
+
+
+
+                })
+
+
+                fila.addEventListener('click', () => {
+                    fetch('https://jsonplaceholder.typicode.com/posts/1/comments') // busca do endereço o comentario dos usuarios
+                        .then((response) => response.json())
+                        .then((json) => console.log(json));
+                    for (let x of Json) {
+                        const comment = document.createElement("p")
+                        comment.innerText = x.userId
+                        comment.classList.add("client")
+                        referencia.append(comment)
+
+                        
+                        
+
+                    }
+
+                })
+
 
             }
         })
+
+
 
 
 };
@@ -21,15 +64,10 @@ verAluno.addEventListener('click', () => {
 
 })
 
-const verAluno1 = document.getElementById("aluno")
-
-fetch('https://jsonplaceholder.typicode.com/posts')
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-
-  const users = document.createElement("p1")
 
 
 
 
-  
+
+
+
